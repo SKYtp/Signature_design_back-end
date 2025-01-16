d9177b5cd7561641c5ebf7d1dfc314e5fb9878ee
Wasan Udomtanasub <poul2545@gmail.com>
2025-01-16 20:13:18 +0700

    improve_check_function

--- src/image_to_value.py
+++ src/image_to_value.py
@@ -4,6 +4,7 @@ from angle_of_signature import angle_of_signature
 from cross_check import get_cross
 
 def final_value_contour_allSig(input_image_path):
+    # return distance, tall, angle
     image_path = input_image_path
     all_points = extract_all_contour_points(image_path)
     group_points = group_cal(all_points)
@@ -11,16 +12,20 @@ def final_value_contour_allSig(input_image_path):
     h_l_n = highest_lowest_nearest(group_points, compare)
     # print('Distance and Tall :', distance_and_tall_of_two_group(h_l_n))
     # print('Angle of signature : ' + str(angle_of_signature(h_l_n)) + '°')
-    return str(distance_and_tall_of_two_group(h_l_n))+", "+ str(angle_of_signature(h_l_n))
+    tmp = distance_and_tall_of_two_group(h_l_n)
+    result = [tmp[0], tmp[1], angle_of_signature(h_l_n)]
+    return result
     
 def final_value_contour_head(input_image_path):
+    # return line_connection, cross_line_number
     image_path = input_image_path
     all_points = extract_all_contour_points(image_path)
     group_points = group_cal(all_points)
     cross_num = get_cross(image_path)
     # print("cross_num: " + str(cross_num))
     # print('Line dis : ' + str(bool(len(group_points) != 1)))
-    return str(bool(len(group_points) != 1)) + ", " + str(cross_num)
+    result = [bool(len(group_points) != 1), cross_num]
+    return result
 
 asign = final_value_contour_allSig('./test_pic/21.png');
 hsign = final_value_contour_head('./test_pic/6.png')
